c3d00eb192f9f6f6be74d1610f6315cd7a7c40f1
David Gergely <dgergely03@gmail.com>
2024-03-26 12:26:09 +0200

    First step: create the table

--- src/app/vms-list/vms-list.component.ts
+++ src/app/vms-list/vms-list.component.ts
@@ -2,17 +2,20 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ContainerComponent } from '../core/container.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-vms-list',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, MatTableModule, MatButtonModule],
   templateUrl: './vms-list.component.html',
   styleUrl: './vms-list.component.scss'
 })
 export class VmsListComponent extends ContainerComponent implements OnInit {
   private readonly httpClient = inject(HttpClient);
   public virtualMachines: any[] = [];
+  public displayedColumns: string[] = ['name', 'lastActivity', 'status', 'actions']
 
   public ngOnInit(): void {
     this.httpClient.get('/api/virtual-machines')
