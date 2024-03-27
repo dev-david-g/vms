be11bed5a7af1e5ae63afddc0fc4ee252d34a590
David Gergely <dgergely03@gmail.com>
2024-03-27 11:29:59 +0200

    Add more example with DI-based and functional interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,21 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { loggingInterceptor } from './core/logging.interceptor';
+import { httpErrorInterceptor } from './core/error.interceptor';
+import { LoggingInterceptor } from './core-old/logging-old.interceptor';
 
-export const appConfig: ApplicationConfig = {providers: 
-  [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(
-    withFetch(), // more modern API to make requests than XMLHttpRequest
-    withInterceptors([loggingInterceptor]))]
+export const appConfig: ApplicationConfig = {
+  providers:
+    [
+      provideRouter(routes),
+      provideAnimationsAsync(),
+      provideHttpClient(
+        withFetch(), // more modern API to make requests than XMLHttpRequest
+        withInterceptors([loggingInterceptor, httpErrorInterceptor]), // define list of interceptors
+        withInterceptorsFromDi(), // allow to import interceptors using DI
+      ),
+      { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true }
+    ]
 };
---
+++ src/app/core-old/logging-old.interceptor.ts
@@ -0,0 +1,11 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class LoggingInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, handler: HttpHandler): Observable<HttpEvent<any>> {
+        console.log('[OLD] ' + req.method + ': ' + req.url);
+        return handler.handle(req);
+    }
+}
