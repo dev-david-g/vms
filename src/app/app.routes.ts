500e8699c0a542676cbf4f443d4571a76ab19fd3
David Gergely <dgergely03@gmail.com>
2024-03-27 11:53:47 +0200

    Set up wildcard route and default page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { VmsListComponent } from './vms-list/vms-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {
         path: 'list',
+        title: 'Virtual machines list',
         loadComponent: () => import('./vms-list/vms-list.component').then(() => VmsListComponent)
+    },
+    {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        title: 'Page not found',
+        loadComponent: () => import('./page-not-found/page-not-found.component').then(() => PageNotFoundComponent)
     }
 ];
