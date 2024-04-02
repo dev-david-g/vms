bf747fc8c3cd230aa966273f25334f86822d4586
David Gergely <dgergely03@gmail.com>
2024-04-02 13:24:42 +0300

    Fix signal thing

--- src/app/vms-details/vms-details-view/vms-details-view.component.ts
+++ src/app/vms-details/vms-details-view/vms-details-view.component.ts
@@ -5,21 +5,20 @@ import { VmsDetailsResourcesComponent } from '../vms-details-resources/vms-detai
   selector: 'app-vms-details-view',
   standalone: true,
   imports: [VmsDetailsResourcesComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './vms-details-view.component.html',
   styleUrl: './vms-details-view.component.scss',
 })
 export class VmsDetailsViewComponent implements OnInit {
   public id = 10;
-  public usedRam = 1024;
-  public usedRamSignal = signal(1024)
+  public usedRamSignal = signal(1024);
   public readonly ramSizes = [512, 1024, 2048, 8000];
 
   public ngOnInit(): void {
     setInterval(() => {
       this.id = this.id + 10;
-      this.usedRam = this.ramSizes[Math.floor(Math.random() * 4)];
       this.usedRamSignal.set(this.ramSizes[Math.floor(Math.random() * 4)]);
-      console.log('used RAM: ' + this.usedRam + ' and id: ' + this.id);
+      console.log('used RAM: ' + this.usedRamSignal() + ' and id: ' + this.id);
     }, 1000)
   }
 }
