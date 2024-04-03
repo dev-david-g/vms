bb4a1d908176b713bffa08bfe3067d20d2e5757b
David Gergely <dgergely03@gmail.com>
2024-04-03 15:39:37 +0300

    Use signals

--- src/app/vms-dashboard/vms-resource-usage/vms-resource-usage.component.ts
+++ src/app/vms-dashboard/vms-resource-usage/vms-resource-usage.component.ts
@@ -1,29 +1,33 @@
-import { ChangeDetectorRef, Component, OnInit, inject, input } from '@angular/core';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, inject, input } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-vms-resource-usage',
   standalone: true,
   imports: [MatProgressBarModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './vms-resource-usage.component.html',
   styleUrl: './vms-resource-usage.component.scss'
 })
-export class VmsResourceUsageComponent implements OnInit {
+export class VmsResourceUsageComponent {
   public virtualMachine = input<any>(0);
   public maxCPU = 3.40;
-  public maxRAM = 2048; 
-  public maxStorage = 128; 
+  public maxRAM = 2048;
+  public maxStorage = 128;
   public usedCPU: number = 0;
   public usedCPUPercentage: number = 0;
   public usedRAM: number = 0;
   public usedRAMPercentage = 0;
   public usedStorage: number = 0;
   public usedStoragePercentage: number = 0;
-  public changeDetectionRef = inject(ChangeDetectorRef);
+
+ public changeDetectionRef = inject(ChangeDetectorRef);
 
   public ngOnInit(): void {
+
     this.calculateValues();
-    setInterval(() => { this.calculateValues(); this.changeDetectionRef.markForCheck();}, 5000);
+
+    setInterval(() => { this.calculateValues(); this.changeDetectionRef.markForCheck(); }, 1000);
   }
 
   private calculateValues(): void {
