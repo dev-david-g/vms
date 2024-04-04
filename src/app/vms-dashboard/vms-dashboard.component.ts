070ed4dc911bba319ce5b90cfa9db550bc3b7795
David Gergely <dgergely03@gmail.com>
2024-04-04 15:53:28 +0300

    Wrong usage of defer

--- src/app/vms-dashboard/vms-dashboard.component.ts
+++ src/app/vms-dashboard/vms-dashboard.component.ts
@@ -4,14 +4,20 @@ import { VmsResourceUsageComponent } from './vms-resource-usage/vms-resource-usa
 import { VmsResourceUsageWithSignalsComponent } from './vms-resource-usage-with-signals/vms-resource-usage-with-signals.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RecentVmsComponent } from './recent-vms/recent-vms.component';
+import {MatSlideToggleChange, MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-vms-dashboard',
   standalone: true,
-  imports: [VmsLogsComponent, VmsResourceUsageComponent, VmsResourceUsageWithSignalsComponent, MatProgressSpinnerModule, RecentVmsComponent],
+  imports: [VmsLogsComponent, VmsResourceUsageComponent, VmsResourceUsageWithSignalsComponent, MatProgressSpinnerModule, RecentVmsComponent, MatSlideToggleModule],
   templateUrl: './vms-dashboard.component.html',
   styleUrl: './vms-dashboard.component.scss'
 })
 export class VmsDashboardComponent {
+  public isSignalComponentVisible = signal(false);
+
+  public signalBasedToggleChange($event: MatSlideToggleChange) {
+      this.isSignalComponentVisible.set($event.checked);
+  }
 
 }
