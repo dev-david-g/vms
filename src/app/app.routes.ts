6330d2c91fa25a584cda4c4b800d7a89aa2f62d8
Alexandru Muresan <muresan.alexandru933@gmail.com>
2024-04-16 12:34:36 +0300

    Here is the built in control flow: Done the following changes: - created examples for the following built in control flow: @for @if @switch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,40 +8,67 @@ import { randomGuard } from './core/guards/random.guard';
 import { dataResolver } from './core/resolvers/data.resolver';
 import { VMS_DETAILS_ROUTES } from './vms-details/vms-details.routes';
 import { VmsDashboardComponent } from './vms-dashboard/vms-dashboard.component';
+import { SwitchExampleComponent } from './built-in-control-flow/switch-example/switch-example.component';
+import { IfExampleComponent } from './built-in-control-flow/if-example/if-example.component';
+import { ForExampleComponent } from './built-in-control-flow/for-example/for-example.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        title: 'Dashboard',
-        loadComponent: () => import('./vms-dashboard/vms-dashboard.component').then(() => VmsDashboardComponent),
-        pathMatch: 'full'
-    },
-    {
-        path: 'login',
-        title: 'Virtual machines service - Login',
-        component: LoginComponent
-    },
-    {
-        path: 'logout',
-        title: 'Virtual machines service - Logout',
-        loadComponent: () => import('./logout/logout.component').then(() => LogoutComponent),
-        canActivate:[authGuard]
-    },
-    {
-        path: 'list',
-        title: 'Virtual machines list',
-        loadComponent: () => import('./vms-list/vms-list.component').then(() => VmsListComponent),
-        canActivate:[authGuard],
-        canMatch: [randomGuard],
-        resolve: [dataResolver]
-    },
-    {
-        path: 'details/:id',
-        loadChildren: () => import('./vms-details/vms-details.routes').then(() => VMS_DETAILS_ROUTES),
-    },
-    {
-        path: '**',
-        title: 'Page not found',
-        loadComponent: () => import('./page-not-found/page-not-found.component').then(() => PageNotFoundComponent)
-    }
+  {
+    path: '',
+    title: 'Dashboard',
+    loadComponent: () =>
+      import('./vms-dashboard/vms-dashboard.component').then(
+        () => VmsDashboardComponent
+      ),
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    title: 'Virtual machines service - Login',
+    component: LoginComponent,
+  },
+  {
+    path: 'switch-example',
+    title: 'Switch-Example',
+    component: SwitchExampleComponent,
+  },
+  {
+    path: 'if-example',
+    title: 'If-Example',
+    component: IfExampleComponent,
+  },
+  {
+    path: 'for-example',
+    title: 'For-Example',
+    component: ForExampleComponent,
+  },
+  {
+    path: 'logout',
+    title: 'Virtual machines service - Logout',
+    loadComponent: () =>
+      import('./logout/logout.component').then(() => LogoutComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'list',
+    title: 'Virtual machines list',
+    loadComponent: () =>
+      import('./vms-list/vms-list.component').then(() => VmsListComponent),
+    canActivate: [authGuard],
+    canMatch: [randomGuard],
+    resolve: [dataResolver],
+  },
+  {
+    path: 'details/:id',
+    loadChildren: () =>
+      import('./vms-details/vms-details.routes').then(() => VMS_DETAILS_ROUTES),
+  },
+  {
+    path: '**',
+    title: 'Page not found',
+    loadComponent: () =>
+      import('./page-not-found/page-not-found.component').then(
+        () => PageNotFoundComponent
+      ),
+  },
 ];
---
+++ src/app/built-in-control-flow/for-example/for-example.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-for-example',
+  standalone: true,
+  imports: [],
+  templateUrl: './for-example.component.html',
+  styleUrl: './for-example.component.scss',
+})
+export class ForExampleComponent {
+  items: string[] = [];
+
+  generateItems() {
+    this.items = ['Bulgaria', 'Romania', 'Italy', 'Finland', 'Austria', 'Norway', 'Germany'];
+  }
+
+  removeItems() {
+    this.items = [];
+  }
+}
